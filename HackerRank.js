f6f37c1220718d78b52860c9d85fd2f0df9fe142
Tomasz Goliński - laptop <tgolinski55@gmail.com>
2019-03-29 13:56:25 +0100

    Add posNeg function

--- HackerRank.js
+++ HackerRank.js
@@ -43,12 +43,12 @@ function nearHundred(n){
 }
 function posNeg(a, b, negative){
 	if(negative)
-		if (!a&&!b)
+		if (a<0&&b<0)
 			return true
 		else
 			return false
 	else 
-		if (!a&&b||a&&!b)
+		if (a<0&&b>0||a>0&&b<0)
 			return true
 		else
 			return false
